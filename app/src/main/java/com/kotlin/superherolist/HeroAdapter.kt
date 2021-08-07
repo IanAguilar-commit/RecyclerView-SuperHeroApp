3b085f63ecb29d2fa6f9dd94531828c96f081fe9
IanAguilar-commit <lishioon@gmail.com>
2021-08-07 00:18:27 -0300

    terminando el recycler

---
+++ app/src/main/java/com/kotlin/superherolist/HeroAdapter.kt
@@ -0,0 +1,39 @@
+package com.kotlin.superherolist
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.kotlin.superherolist.databinding.ActivityMainBinding
+import com.kotlin.superherolist.databinding.ItemSuperheroBinding
+import com.squareup.picasso.Picasso
+
+class HeroAdapter (val superhero:List<SuperHero>):RecyclerView.Adapter<HeroAdapter.HeroHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HeroHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return HeroHolder(layoutInflater.inflate(R.layout.item_superhero, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: HeroHolder, position: Int) {
+
+        holder.render(superhero[position])
+    }
+
+    override fun getItemCount(): Int = superhero.size
+    
+    class HeroHolder(val view: View):RecyclerView.ViewHolder(view){
+
+            val binding = ItemSuperheroBinding.bind(view)
+
+            fun render(superhero: SuperHero){
+            binding.tvRealName.text = superhero.realName
+            binding.tvSuperHeroName.text = superhero.superHeroName
+            binding.tvPublisher.text = superhero.publisher
+        
+            Picasso.get().load(superhero.image).into(binding.ivHero)
+        
+        
+                }
+    }
+}
